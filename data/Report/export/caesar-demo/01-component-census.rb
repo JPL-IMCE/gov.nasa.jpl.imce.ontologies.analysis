eacd717385ea8bf52acfd616eaf5fec6a56ad3c6
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2018-02-06 10:24:36 -0800

    IMCEIS-1709 reports added

---
+++ data/Report/export/caesar-demo/01-component-census.rb
@@ -0,0 +1,19 @@
+name '01 component census'
+
+query %q{
+
+  <%= @namespace_defs %>
+
+  SELECT ?c ?v
+    WHERE {
+    ?c rdfs:subClassOf mission:Component .
+    OPTIONAL {
+      ?c rdfs:subClassOf [ owl:onProperty base:hasCanonicalName ; owl:hasValue ?v ] 
+    }
+    FILTER (
+      REGEX(STR(?c),
+        "http://europa.jpl.nasa.gov/projects/EuropaClipper/DesignCapture/EuropaDesignCapture/EuropaFlightSystem/Products/"
+      )
+    )
+  }
+}
---
+++ data/Report/export/caesar-demo/03-authority-census.rb
@@ -0,0 +1,19 @@
+name '03 authority census'
+
+query %q{
+
+  <%= @namespace_defs %>
+
+  SELECT ?c ?v
+  WHERE {
+    ?c rdfs:subClassOf project:Authority .
+    OPTIONAL {
+      ?c rdfs:subClassOf [ owl:onProperty base:hasCanonicalName ; owl:hasValue ?v ] 
+    }
+    FILTER (
+      REGEX(STR(?c),
+        "http://europa.jpl.nasa.gov/projects/EuropaClipper/DesignCapture/EuropaProjectFramework/EuropaWorkBreakdownStructure/"
+      )
+    )
+  }
+}
