5201382adff4d8e7b46d04fcb63f3da86e7719b7
Joanna Liu <cheng.liu@jpl.nasa.gov>
2018-03-27 16:34:13 -0700

    fix prefix script

--- workflow/load-prefix.sh
+++ workflow/load-prefix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ## load prefixes for running reports
-FUSEKI_S_PUT= $(FUSEKI_BIN)/s-put
-$(FUSEKI_S_PUT) 'http://$(JENA_HOST):$(JENA_PORT)/$(JENA_DATASET)/data' 'default' 'prefix-imports.ttl'
+echo "FUSEKI_BIN: $FUSEKI_BIN"
+
+$FUSEKI_BIN/s-put 'http://'${JENA_HOST}':'${JENA_PORT}'/'${JENA_DATASET}'/data' 'default' 'prefix-imports.ttl'
