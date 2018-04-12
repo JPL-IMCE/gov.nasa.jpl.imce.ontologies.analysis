cee777b89ebfc75b127c029d6ab17012fbb07d9b
Joanna Liu <cheng.liu@jpl.nasa.gov>
2018-04-12 11:08:03 -0700

    scripts cleanup

--- scripts/oml-conversion.sh
+++ scripts/oml-conversion.sh
@@ -23,34 +23,13 @@ HERE="$(pwd)"
 CONVERTER_INFO="$($TOP/target/OMLConverters/bin/omlConverter --version)"
 
 OML_IMPORT=$TOP/target/import
-#PUBLIC_IMPORT=$TOP/target/import-vocabulary
 
 CATALOG=oml.catalog.xml
 INPUT=oml-input
 OUTPUT=ontologies
 
-#PUBLIC=$PUBLIC_IMPORT/$2
-#PUBLIC_ONTOLOGIES=$PUBLIC/ontologies
-#PUBLIC_BUNDLES=$PUBLIC/bundles
-
 IMCE=imce.jpl.nasa.gov
 PROJECT_BUNDLE_PATH=$IMCE/foundation/project
-OMG_ORG=www.omg.org
-
-OMIT="
-  $INPUT/$IMCE/math
-  $INPUT/$IMCE/skeleton
-  $INPUT/$IMCE/discipline/mechanical
-  $INPUT/$IMCE/discipline/ontological-behavior
-  $INPUT/$IMCE/discipline/risk
-  $INPUT/$IMCE/discipline/state-analysis
-  $INPUT/$IMCE/foundation/owl2-mof2
-  $INPUT/$IMCE/foundation/time
-  $INPUT/$IMCE/oml/provenance
-  $INPUT/$IMCE/$OMG_ORG
-  $INPUT/$IMCE/*/*/*-embedding.oml
-  $INPUT/$IMCE/vocabulary
-"
 
 echo "# current path: $(pwd)"
 
@@ -61,28 +40,14 @@ mkdir $INPUT
 rsync -av $OML_IMPORT/$1/resources $INPUT
 rsync -av $OML_IMPORT/$1/bundles $INPUT
 
-# omit unused vocabulary (workaround)
-#rm -rf $OMIT
-
-# remove bogus vocabulary extensions (workaround)
-
-#for o in $(find $INPUT -name '*.oml')
-#do
-#    sed -i.bak '/extends.*owl2-mof2/d' $o
-#done
-
 echo "# converter input path: $INPUT"
 echo "# converter output path: $OUTPUT"
 
 "$TOP/target/OMLConverters/bin/omlConverter" text $INPUT/resources/$CATALOG --output $OUTPUT --owl --clear
 
-# overwrite vocabulary with latest OWL files
-#rsync -av $PUBLIC_ONTOLOGIES/$IMCE $PUBLIC_ONTOLOGIES/$PURL_ORG $PUBLIC_ONTOLOGIES/$W3_ORG $OUTPUT
-
 # add cached project bundle
 
 #mkdir -p $OUTPUT/$PROJECT_BUNDLE_PATH
-#rsync -av --exclude='**-embedding*' $PUBLIC_BUNDLES/$PROJECT_BUNDLE_PATH/ $OUTPUT/$PROJECT_BUNDLE_PATH
 rsync -av --exclude='**-embedding*' $INPUT/bundles/$PROJECT_BUNDLE_PATH/ $OUTPUT/$PROJECT_BUNDLE_PATH
 
 
