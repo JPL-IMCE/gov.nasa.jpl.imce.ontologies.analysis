6a82213a9699975d881bf472623f14701ffa5b8b
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2018-08-28 12:48:19 -0700

    ci.scripts#22 complete

--- workflow/env.sh
+++ workflow/env.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+unset fuseki_protocol fuseki_host fuseki_port fuseki_dataset
+while test $# -gt 0
+do
+    case $1 in
+	--fuseki-protocol)      fuseki_protocol=$2;;
+	--fuseki-host)          fuseki_host=$2;;
+	--fuseki-port)          fuseki_port=$2;;
+	--fuseki-dataset)       fuseki_dataset=$2;;
+    esac
+    shift; shift
+done
+
+export FUSEKI_PROTOCOL=${fuseki_protocol:-http:}
+export FUSEKI_HOST=${fuseki_host:-localhost}
+export FUSEKI_PORT=${fuseki_port:-8898}
+export FUSEKI_DATASET=${fuseki_dataset:-europa-efse}
+
 # Read-only
 export ETC=$(dirname $(pwd))/etc
 
@@ -33,7 +50,7 @@ export FUSEKI_BASE=$(dirname $(pwd))/target/run
 [ ! -d $WORKFLOW/artifacts ] && mkdir -p $WORKFLOW/artifacts
 
 export RUBYLIB=$TOOLS/lib/Application:\
-$TOOLS/lib/Audit:\
+$tools/lib/Audit:\
 $TOOLS/lib/BundleClosure:\
 $TOOLS/lib/IMCE:\
 $TOOLS/lib/Jena:\
@@ -51,28 +68,12 @@ $TOOLS/lib/OMFMetadata
 # && exit -1
 
 [ -z "$JRUBY" ] && export JRUBY=$(which jruby)
-echo "# JRUBY=$JRUBY"
 
 export GEM_PATH="${GEM_HOME}:$(dirname $(pwd))"
 
 export PARALLEL_MAKE_OPTS="-j8 -l16"
 
-JENA_DATASET_NAME="europa-efse"
-if [ $# -gt 0 ]; then
-  JENA_DATASET_NAME="$1"
-fi
-export JENA_DATASET=$JENA_DATASET_NAME
-echo "JENA_DATASET is ${JENA_DATASET}"
-
-export JENA_HOST="localhost"
-
-JENA_PORT_NUMBER="8898"
-
-if [ $# -gt 1 ]; then
-  JENA_PORT_NUMBER="$2"
-fi
-export JENA_PORT=$JENA_PORT_NUMBER
-echo "JENA_PORT is ${JENA_PORT}"
+export FUSEKI_ENDPOINT=$FUSEKI_PROTOCOL//$FUSEKI_HOST:$FUSEKI_PORT/$FUSEKI_DATASET
 
 # Add as maven dependency
 export DOCBOOK_XHTML_XSL="${TOOLS}/docbook/xhtml/docbook.xsl"
@@ -82,11 +83,9 @@ if [ $# -gt 2 ] && [ $3 != 'undefined' ]; then
    AUDITS_TREE_PATH="$3"
 fi
 export AUDITS_TREE_PATH
-echo "AUDITS_TREE_PATH is ${AUDITS_TREE_PATH}"
 
 REPORTS_TREE_PATH=$(dirname $(pwd))/data/Report/export/caesar-demo
 if [ $# -gt 3 ] && [ $4 != 'undefined' ]; then
     REPORTS_TREE_PATH="$4"
 fi
 export REPORTS_TREE_PATH
-echo "REPORTS_TREE_PATH is ${REPORTS_TREE_PATH}"
